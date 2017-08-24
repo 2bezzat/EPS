0c1ab3d056daa63846c418fe0c34d2965bb2b217
Abd El Rahman Ezzat <abdo.ezzat@thed.io>
2017-08-24 19:49:26 +0200

    Finishing and wrapping up[

---
+++ eps_2017-08-24.sql
@@ -0,0 +1,106 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.6.33)
+# Database: eps
+# Generation Time: 2017-08-24 17:40:24 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table accounts
+# ------------------------------------------------------------
+
+CREATE TABLE `accounts` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` longtext COLLATE utf8_unicode_ci,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Dump of table migrations
+# ------------------------------------------------------------
+
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Dump of table password_resets
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `password_resets`;
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`),
+  KEY `password_resets_token_index` (`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+# Dump of table users_account_types
+# ------------------------------------------------------------
+
+CREATE TABLE `users_account_types` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned DEFAULT NULL,
+  `account_id` int(10) unsigned DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `users_account_types_user_id_foreign` (`user_id`),
+  KEY `users_account_types_account_id_foreign` (`account_id`),
+  CONSTRAINT `users_account_types_account_id_foreign` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`),
+  CONSTRAINT `users_account_types_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,54 @@
 @section('main')
 
 <h1>Welcome to the EPS TASK</h1>
-<p class="lead">PUT YOUR INFO ABOUT THE PROJECT HERE DON:T FORGET</p>
+<h3>Intro:</h3>
+
+<p>
+	Hello, This is the task that was asked from me "AbdElRahman Ezzat - a.ezzat1997@gmail.com".
+	The task was Requested by Dr Gamal & Dr Abd EL Qader.
+	The Task bussiness is that we have an accounts (maybe bank account "The Task doucment wasn't clear about it") and to create tasks and assign it to users.
+</p>
+<hr>
+<p>
+	<i>My Approach to this task is.</i>
+</p>
+<p>
+	I First made the user and useController files to be able to create and edit the user. <br>
+	I then added the accounts and accoutController to add the accounts types. <br>
+	I used Many to Many relationships to link the users with the accounts, as the user may have many account and the account itself may belong to many user.
+</p>
+
+
+
+<h3>Notes </h3>
+<ul>
+	<li>
+		Edit .env file to match the database credintails to your record
+	</li>
+	<li>Run "php artisan migrate" to include the database design implemnted</li>
+	<li>
+		there is a dump of the data incase you want a ready data ie(data while working, as I didn't have time to make some factories)
+	</li>
+	<li>
+		The Files that were created/Edited are 
+		<ul>
+			<li>
+				Web (routes files)
+			</li>
+			<li>userController</li>
+			<li>acountController</li>
+			<li>UserRequest [to validate the User form]</li>
+			<li>AccountRequest [to validate the Account form]</li>
+			<li>The naming conventions is following  PSR-2</li>
+		</ul>
+	</li>
+	<li>
+		The used Packages are the following
+		<ul>
+			<li>
+				Laravel Collections (for deailing with forms)
+			</li>
+		</ul>
+	</li>
+</ul>
 @stop
